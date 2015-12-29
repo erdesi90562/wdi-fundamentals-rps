aef69ca3d28bc9e02829d0d740d006cf4d52aea7
Ethan D <erdesi90562@gmail.com>
2015-12-28 21:46:03 -0500

    Wrote getWinner Function Dirty

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,6 +41,23 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove === computerMove) {
+        winner = "It's a TIE!!!"
+    }else if ((playerMove === "rock") && (computerMove === "scissors")) {
+        winner = "player";
+    } else if ((playerMove === "scissors") && (computerMove === "paper")) {
+        winner = "player";
+    } else if ((playerMove === "paper") && (computerMove === "rock")) {
+        winner = "player";
+    } else if ((computerMove === "rock") && (playerMove === "scissors")) {
+        winner = "computer";
+    } else if ((computerMove === "scissors") && (computerMove === "paper")) {
+        winner = "computer";
+    } else if ((computerMove === "paper") && (playerMove === "rock")) {
+        winner = "computer"
+    } else {
+        winner = "error!!!!!"
+    }
     return winner;
 }
 
@@ -55,4 +72,36 @@ function playToFive() {
 
 /*  OK so this is from code academy use as ref */
 
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
 
+
+var compare = function(choice1,choice2) {
+    if (choice1 === choice2) {
+        return "The result is a tie!";
+    }    
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "rock wins";
+        }
+        else {
+            return "paper wins";
+        }
+    }
+    
+else if (choice1 === "paper") {
+    if (choice2 === "rock") {
+    
+        return "paper wins";
+    } else {
+        return "scissors wins";
+    }
+    }
+}
